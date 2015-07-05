80c7808b026a367449da103123a896311f27f1bd
ys1382 <baba@Suleymans-MacBook-Air.local>
2015-07-04 19:45:17 -0700

    push works

--- ClearKeep/ClearKeep-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-#ifndef ClearKeep_Bridging_Header_h
-#define ClearKeep_Bridging_Header_h
-
-#import <CommonCrypto/CommonKeyDerivation.h>
-#import <CommonCrypto/CommonCryptor.h>
-
-#endif /* ClearKeep_Bridging_Header_h */
--- node/testcurl.sh
+++
@@ -1,2 +0,0 @@
-#register x/y
-curl -XPOST -H'Content-Type: application/json' -d'{"username":"x","password":"y"}' http://127.0.0.1:3000/register
