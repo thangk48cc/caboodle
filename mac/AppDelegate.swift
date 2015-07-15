62b6f705705abe99d259e93a86523def8ce35ed3
ys1382 <ys1382@gmail.com>
2015-07-15 15:12:55 -0700

    mac

---
+++ mac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  mac
+//
+//  Created by baba on 7/15/15.
+//  Copyright © 2015 ck. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ mac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mac
+//
+//  Created by baba on 7/15/15.
+//  Copyright © 2015 ck. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
